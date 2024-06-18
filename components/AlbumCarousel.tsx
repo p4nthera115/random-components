41f7d872f1b824b3454cc2940db8ac3c05da2a29
p4nthera115 <asdshuja@hotmail.co.uk>
2024-06-18 18:33:52 +0200

    animated gradient

--- components/AlbumCarousel.tsx
+++ components/AlbumCarousel.tsx
@@ -1,20 +1,60 @@
+"use client";
+
 import Image from "next/image";
+import { useState } from "react";
+import { motion } from "framer-motion";
 
 export default function AlbumCarousel() {
+  const [active, setActive] = useState(false);
+  const [color, setColor] = useState("#A4998C");
+
   const albumObj = [
-    { name: "Section.80", year: "2011", cover: "/section80.jpg" },
-    { name: "good kid, m.A.A.d city", year: "2012", cover: "/gkmc.jpg" },
-    { name: "To Pimp a Butterfly", year: "2015", cover: "/tpab.webp" },
-    { name: "DAMN.", year: "2016", cover: "/damn.png" },
+    {
+      name: "Section.80",
+      year: "2011",
+      cover: "/section80.jpg",
+      color: "#535B4B",
+    },
+    {
+      name: "good kid, m.A.A.d city",
+      year: "2012",
+      cover: "/gkmc.jpg",
+      color: "#4280A7",
+    },
+    {
+      name: "To Pimp a Butterfly",
+      year: "2015",
+      cover: "/tpab.webp",
+      color: "#565656",
+    },
+    {
+      name: "DAMN.",
+      year: "2016",
+      cover: "/damn.png",
+      color: "#ED0D0D",
+    },
     {
       name: "Mr. Morale & the Big Steppers",
       year: "2022",
       cover: "/mmatbs.webp",
+      color: "#89734C",
     },
   ];
+
   return (
     <>
-      <div className="bg-gradient-to-b from-[#A4998C] to-transparent absolute h-full w-full"></div>
+      <motion.div
+        initial={{
+          backgroundImage: `linear-gradient(${color}, rgba(255,0,0,0))`,
+        }}
+        animate={{
+          backgroundImage: `linear-gradient(${color}, rgba(255,0,0,0))`,
+        }}
+        className={`${color} absolute h-full w-full`}
+      ></motion.div>
+
+      <linearGradient />
+
       <div className="p-6 flex flex-col h-full w-full gap-6 z-20">
         <section className="h-[40%] flex flex-row gap-10 p-8 rounded-3xl bg-[#d3ccc3]/50 items-center ">
           <div className="h-full bg-black rounded-3xl aspect-square flex relative ">
@@ -25,16 +65,20 @@ export default function AlbumCarousel() {
               className="object-cover rounded-3xl"
             />
           </div>
-          {/* <div className="rounded-3xl w-full flex-row p-10 gap-10 bg-black flex overflow-hidden relative items-center"> */}
           <h1 className="h-1/4 text-8xl font-bold flex items-center z-20 text-white/95">
             Kendrick Lamar
           </h1>
-          {/* </div> */}
         </section>
 
         <section className=" rounded-3xl h-3/4 inline-flex flex-row p-10 gap-12 w-full justify-evenly items-center">
           {albumObj.map((album, i) => (
-            <article className="rounded-lg bg-white/20 p-4 w-72 max-h-96 inline-flex flex-col gap-4">
+            <article
+              onClick={() => {
+                setActive(true);
+                setColor(album.color);
+              }}
+              className="rounded-lg bg-white/20 p-4 w-72 max-h-96 inline-flex flex-col gap-4"
+            >
               <div className="h-64 w-64 bg-black rounded relative">
                 <Image src={album.cover} alt={album.name} fill />
               </div>
